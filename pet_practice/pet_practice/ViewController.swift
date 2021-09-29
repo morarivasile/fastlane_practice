59a5306e5756b1e6883eca84ae66e8c886f366a5
Vasile Morari <xopow@outlook.com>
2021-09-30 01:39:21 +0300

    Added template project

---
+++ pet_practice/pet_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pet_practice
+//
+//  Created by Vasile Morari on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
