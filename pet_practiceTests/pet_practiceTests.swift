4342d9332fe9d2aff785db52c1406619432374b4
Vasile Morari <xopow@outlook.com>
2021-09-30 02:01:46 +0300

    added ui tests bundle

--- pet_practiceTests/pet_practiceTests.swift
+++ pet_practiceTests/pet_practiceTests.swift
@@ -17,16 +17,18 @@ class pet_practiceTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testUnitExample1() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testUnitExample2() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-
+    
+    func testUnitExample3() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
 }
